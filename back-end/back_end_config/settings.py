4717afbf0c6bb0795029b7e2d100b8f93a2e6c4f
pradobanz <pradobanz@gmail.com>
2024-02-16 20:03:22 -1000

    Last commit for deployment

--- back-end/back_end_config/settings.py
+++ back-end/back_end_config/settings.py
@@ -23,11 +23,11 @@ env = dotenv_values(".env")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.get("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS =[""]
 # Application definition
 
 INSTALLED_APPS = [
